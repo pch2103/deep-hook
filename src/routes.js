a9be4467644c5115e8246b93368587daea9e6d22
pch2013 <pch2103@gmail.com>
2020-05-01 17:57:28 +0300

    FINAL

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Switch, Route, HashRouter} from 'react-router-dom'
+import {Switch, Route, BrowserRouter} from 'react-router-dom'
 import GlobalFeed from "./pages/globalFeed/GlobalFeed"
 import Article from "./pages/article/Article"
 import Auth from "./pages/auth/Auth"
@@ -12,22 +12,22 @@ import UserProfile from "./pages/userProflle/UserProfile";
 
 const Routes = () => {
 	return (
-			<HashRouter basename="/">
-		<Switch>
-			<Route path='/' component={GlobalFeed} exact />
-			<Route path='/profiles/:slug' component={UserProfile} exact/>
-			<Route path='/profiles/:slug/favorites' component={UserProfile} />
-			<Route path='/settings' component={Settings} />
-			<Route path='/articles/new' component={CreateArticle} />
-			<Route path='/articles/:slug/edit' component={EditArticle} />
-			<Route path='/feed' component={YourFeed} />
-			<Route path='/tags/:slug' component={TagFeed} />
-			<Route path='/login' component={Auth} />
-			<Route path='/register' component={Auth} />
-			<Route path='/articles/:slug' component={Article} />
+			<BrowserRouter basename='/deep-hook'>
+				<Switch>
+					<Route path='/' component={GlobalFeed} exact/>
+					<Route path='/profiles/:slug' component={UserProfile} exact/>
+					<Route path='/profiles/:slug/favorites' component={UserProfile}/>
+					<Route path='/settings' component={Settings}/>
+					<Route path='/articles/new' component={CreateArticle}/>
+					<Route path='/articles/:slug/edit' component={EditArticle}/>
+					<Route path='/feed' component={YourFeed}/>
+					<Route path='/tags/:slug' component={TagFeed}/>
+					<Route path='/login' component={Auth}/>
+					<Route path='/register' component={Auth}/>
+					<Route path='/articles/:slug' component={Article}/>
+				</Switch>
+			</BrowserRouter>
 
-		</Switch>
-			</HashRouter>
 	);
 }
 
